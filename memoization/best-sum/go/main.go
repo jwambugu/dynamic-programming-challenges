ff3088597d9116f0f572659380c611193996023b
Joram Wambugu <artisan254@gmail.com>
2021-07-27 14:36:01 +0300

    Best Sum
    Add all possible combinations to give the sum of the value.

--- memoization/best-sum/go/main.go
+++ memoization/best-sum/go/main.go
@@ -4,11 +4,9 @@ import "fmt"
 
 type memo map[int][]int
 
-type possibleCombination struct {
-	combination []int
-}
+type combinations [][]int
 
-func bestSum(targetSum int, numbers []int, m memo) ([]int, []possibleCombination) {
+func bestSum(targetSum int, numbers []int, m memo) ([]int, combinations) {
 	if _, exists := m[targetSum]; exists {
 		return m[targetSum], nil
 	}
@@ -22,7 +20,7 @@ func bestSum(targetSum int, numbers []int, m memo) ([]int, []possibleCombination
 	}
 
 	var shortestCombination []int
-	var possibleCombinations []possibleCombination
+	var possibleCombinations combinations
 
 	for _, number := range numbers {
 		remainder := targetSum - number
@@ -33,10 +31,7 @@ func bestSum(targetSum int, numbers []int, m memo) ([]int, []possibleCombination
 			combination := append(remainderCombination, number)
 
 			if shortestCombination == nil || len(combination) < len(shortestCombination) {
-
-				possibleCombinations = append(possibleCombinations, possibleCombination{
-					combination: combination,
-				})
+				possibleCombinations = append(possibleCombinations, combination)
 
 				shortestCombination = combination
 			}
@@ -49,8 +44,9 @@ func bestSum(targetSum int, numbers []int, m memo) ([]int, []possibleCombination
 }
 
 func main() {
-	fmt.Println(bestSum(7, []int{5, 3, 4, 7}, memo{}))    // [7]
-	fmt.Println(bestSum(8, []int{2, 3, 5}, memo{}))       // [3,5]
-	fmt.Println(bestSum(8, []int{1, 4, 5}, memo{}))       // [4,4]
-	fmt.Println(bestSum(100, []int{1, 2, 5, 25}, memo{})) // [25,25,25,25]
+	fmt.Println(bestSum(7, []int{5, 3, 4, 7}, memo{})) // [7] [[4 3] [7]]
+	fmt.Println(bestSum(8, []int{2, 3, 5}, memo{}))    // [5 3] [[3 3 2] [5 3]]
+	fmt.Println(bestSum(8, []int{1, 4, 5}, memo{}))    // [4 4] [[5 1 1 1] [4 4]]
+	fmt.Println(bestSum(100, []int{1, 2, 5, 25}, memo{}))
+	// [25 25 25 25] [[25 25 25 5 5 5 5 5 2 1] [25 25 25 5 5 5 5 5] [25 25 25 25]]
 }
