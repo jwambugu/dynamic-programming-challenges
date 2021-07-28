f19374bb72dbd8fb3958ff7ff65563088fb04254
Joram Wambugu <artisan254@gmail.com>
2021-07-28 15:44:43 +0300

    All Construct: solve using js

--- memoization/all-construct/js/index.js
+++ memoization/all-construct/js/index.js
@@ -1,35 +1,35 @@
 const allConstruct = (target, wordBank, memo = {}) => {
-    if (target in memo) {
-        return memo[target];
-    }
-
-    if (target === "") {
-        return [[]];
-    }
-
-    const result = [];
-
-    for (let word of wordBank) {
-        if (target.indexOf(word) === 0) {
-            const suffix = target.slice(word.length);
-
-            const possibleSuffixWays = allConstruct(suffix, wordBank, memo);
-
-            const targetWays = possibleSuffixWays.map((way) => [word, ...way]);
-
-            result.push(...targetWays);
-        }
-    }
-
-    memo[target] = result;
+  if (target in memo) {
     return memo[target];
+  }
+
+  if (target === "") {
+    return [[]];
+  }
+
+  const result = [];
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+
+      const possibleSuffixWays = allConstruct(suffix, wordBank, memo);
+
+      const targetWays = possibleSuffixWays.map((way) => [word, ...way]);
+
+      result.push(...targetWays);
+    }
+  }
+
+  memo[target] = result;
+  return memo[target];
 };
 
 console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
 // [ [ 'purp', 'le' ], [ 'p', 'ur', 'p', 'le' ] ]
 
 console.log(
-    allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"])
+  allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"])
 );
 
 // [
@@ -40,17 +40,17 @@ console.log(
 // ]
 
 console.log(
-    allConstruct("skateboard", ["bo", "rd", "ate", "ska", "sk", "boar"])
+  allConstruct("skateboard", ["bo", "rd", "ate", "ska", "sk", "boar"])
 );
 // []
 
 console.log(
-    allConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaaz", [
-        "a",
-        "aa",
-        "aaa",
-        "aaaa",
-        "aaaaa",
-    ])
+  allConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaaz", [
+    "a",
+    "aa",
+    "aaa",
+    "aaaa",
+    "aaaaa",
+  ])
 );
 // []
